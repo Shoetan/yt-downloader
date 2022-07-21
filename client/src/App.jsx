a29c314192051b52c49dac5b80981fe4c09f143b
unknown <emmysoetan@gmail.com>
2022-07-21 16:56:55 +0100

    Created front end for the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
 const App = () => {
   return ( 
     <div>
-      Hello 
+      <div className="container mx-auto flex justify-center gap-2 mt-10">
+        <input type="text" 
+          placeholder="url..."
+          className="font_poppins rounded-sm h-11 w-96 p-6 focus:outline-none"
+        />
+        <button className='font-poppins bg-slate-400 p-2 rounded-md'>Download</button>
+      </div>
     </div>
    );
 }
